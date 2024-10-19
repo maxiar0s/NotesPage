2781575dc2cb7fa8c40b2903de1e03ccf0b3e739
maxiar0s <maxi.aros.osorio@gmail.com>
2024-10-18 21:37:47 -0300

    fix

--- mongo.js
+++ mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2];
 
-const url = `mongodb+srv://admin:${password}@cluster0.nwezd.mongodb.net/notesApp?retryWrites=true&w=majority&appName=Cluster0`;
+const url = `mongodb+srv://admin:${password}@cluster0.nwezd.mongodb.net/notesDB?retryWrites=true&w=majority&appName=Cluster0`;
 
 mongoose.set("strictQuery", false);
 mongoose.connect(url).then(() => {
